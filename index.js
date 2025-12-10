ce524dfb8a379107591deeaef42574612a6a3db4
MehakSetia <156164684+MehakSetia@users.noreply.github.com>
2025-12-10 12:41:07 +0530

    Added Swagger documentation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocument = yaml.load('./swagger.yaml');
+
+
 const express=require('express');
 const app=express();
 const port=3000;
@@ -10,6 +15,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to the Finance Tracker API! 🚀");
 });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth',authRoutes);
 app.use('/transaction',transRoutes);
 
